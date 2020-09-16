90e7e0b988753615a1eb55dcd252062c07f65a3d
jackindisguise <incognitus.me@gmail.com>
2020-09-16 01:24:35 -0500

    chore: fixed whitespace.

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -6,7 +6,7 @@ export const logger: winston.Logger = winston.createLogger({
 			level: "silly",
 			format: winston.format.combine(
 				winston.format.timestamp(),
-				winston.format.printf(function(i) { return `[${i.timestamp}] <${i.level}> ${i.message}`; })	
+				winston.format.printf(function(i) { return `[${i.timestamp}] <${i.level}> ${i.message}`;})	
 			),
 		}),
 		new (winston.transports.File)({
@@ -14,7 +14,7 @@ export const logger: winston.Logger = winston.createLogger({
 			level: "silly",
 			format: winston.format.combine(
 				winston.format.timestamp(),
-				winston.format.printf(function(i) { return `[${i.timestamp}] <${i.level}> ${i.message}`; })	
+				winston.format.printf(function(i) { return `[${i.timestamp}] <${i.level}> ${i.message}`;})	
 			),
 		})
 	]
