581fa62b886ecda114f7d39b320bba72373be647
jackindisguise <incognitus.me@gmail.com>
2020-09-16 01:23:05 -0500

    chore: setup.spec.ts can't silence logger for testing purposes

--- setup.spec.ts
+++ setup.spec.ts
@@ -1,4 +1,4 @@
 // first test that runs
 // setup the environment for further testing
 import { logger } from "./src/util/logger";
-logger.transports[0].silent = true; // silence console transport
+//logger.transports[0].silent = true; // silence console transport
